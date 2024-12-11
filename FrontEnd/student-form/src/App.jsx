20993590a4801dfdbc9578031fdeb9f02d4b9c18
LaRasa99 <134576794+LRManamperi@users.noreply.github.com>
2024-12-11 08:41:57 +0530

    Popups Done

--- FrontEnd/student-form/src/App.jsx
+++ FrontEnd/student-form/src/App.jsx
@@ -1,8 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 import Header from './Components/Header.jsx';
 import Footer from './Components/Footer.jsx';
-import Home from './Pages/Home.jsx';
-import Form from './Pages/Form.jsx';
+//import Home from './Pages/Home.jsx';
+//import Form from './Pages/Form.jsx';
+import SuccessPopup from './PopUps/Sucessful.jsx';
+import UpdatedPopup from './PopUps/Updated.jsx';
+import DeletedPopup from './PopUps/Deleted.jsx'
 
 
 import './App.css';
@@ -12,9 +15,12 @@ function App() {
         <Router>
             <Header />
             <Routes>
-                <Route path="/" element={<Home />} />
+                {/* <Route path="/" element={<Home />} />
                 <Route path="/home" element={<Home />} />
-                <Route path="/register" element={<Form />} />
+                <Route path="/register" element={<Form />} /> */}
+                {/* <Route path="/" element={<SuccessPopup/>} /> */}
+                {/* <Route path="/" element={<UpdatedPopup/>} /> */}
+                <Route path="/" element={<DeletedPopup/>} />
             </Routes>
             <Footer />
         </Router>
@@ -22,3 +28,32 @@ function App() {
 }
 
 export default App;
+
+
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+// import Header from './Components/Header.jsx';
+// import Footer from './Components/Footer.jsx';
+// import SuccessPopup from './PopUps/Sucessful.jsx';
+// import './App.css';
+
+// function App() {
+//     return (
+//         <Router>
+//             <div className="app-container">
+//                 {/* Header will be visible on all pages */}
+//                 <Header />
+//                 <main className="content">
+//                     <Routes>
+//                         {/* Define routes here */}
+//                         <Route path="/" element={<SuccessPopup />} />
+//                     </Routes>
+//                 </main>
+//                 {/* Footer will be visible on all pages */}
+//                 <Footer />
+//             </div>
+//         </Router>
+//     );
+// }
+
+// export default App;
